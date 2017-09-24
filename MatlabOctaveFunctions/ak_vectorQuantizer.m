044dc67390f367b26566136680c4cc6a6ab987ac
Aldebaro <aldebaro@ufpa.br>
2017-09-24 17:10:14 -0300

    Vector quantizer using MSE

---
+++ MatlabOctaveFunctions/ak_vectorQuantizer.m
@@ -0,0 +1,15 @@
+function bestCodewordIndex = ak_vectorQuantizer(vector, codebook)
+% function bestCodewordIndex = ak_vectorQuantizer(vector, codebook)
+%Encode input vector using N x K codebook matrix using the Euclidean
+%distance (squared error). It outputs the best codeword index.
+[numOfCodewords,K]=size(codebook);
+minimumDistance = Inf; %initialize with large number
+bestCodewordIndex = -1; %initialize with invalid number
+for j=1:numOfCodewords %search all codewords in codebook
+    error=vector-codebook(j,:); % error
+    squaredError=sum(error.*error); %squared Euclidean distance
+    if squaredError < minimumDistance %update
+        bestCodewordIndex = j;
+        minimumDistance = squaredError; %update the best
+    end
+end
