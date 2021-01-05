09cfa2bbe1b350513e019e49e9b9584a842329b9
1176325 <1176325@gitlab.lasse.ufpa.br>
2021-01-05 17:19:37 -0300

    shorter commands

--- MatlabOctaveCodeSnippets/snip_frequency_analysis.m
+++ MatlabOctaveCodeSnippets/snip_frequency_analysis.m
@@ -1,8 +1,8 @@
 N=256; n=0:N-1; %N is the number of available samples and FFT length
-x2=100*cos((2*pi*37.5/N)*n+pi/4) + cos((2*pi*32/N)*n+pi/3); %two cosines
+x2=100*cos((2*pi*37.5/N)*n+pi/4) + cos((2*pi*32/N)*n+pi/3); %cosines
 dw=(2*pi)/N; %DFT spacing in radians
 w=-pi:dw:pi-dw; %abscissa for plots matched to fftshift
 x=x2.*hamming(N)'; %perform windowing
-factor=max(abs(fft(x))); %normalization to have stronger cosine at 0 dB
+factor=max(abs(fft(x))); %normalization to have stronger at 0 dB
 plot(w,fftshift(20*log10(abs(fft(x)/factor))));
 
