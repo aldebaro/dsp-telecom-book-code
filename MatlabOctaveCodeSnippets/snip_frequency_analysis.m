7f7e9559643114cf84eb6bb90c2a101d7dd23d85
1176325 <1176325@gitlab.lasse.ufpa.br>
2021-01-05 10:34:36 -0300

    Use N instead of 256

--- MatlabOctaveCodeSnippets/snip_frequency_analysis.m
+++ MatlabOctaveCodeSnippets/snip_frequency_analysis.m
@@ -1,5 +1,5 @@
 N=256; n=0:N-1; %N is the number of samples available of x1 and x2
-x2=100*cos((2*pi*37.5/256)*n+pi/4) + cos((2*pi*32/N)*n+pi/3); %x2[n]
+x2=100*cos((2*pi*37.5/N)*n+pi/4) + cos((2*pi*32/N)*n+pi/3); %x2[n]
 dw=(2*pi)/N; %DFT spacing in rads
 w=-pi:dw:pi-dw; %abscissa for plots
 x=x2.*hamming(N)'; %perform windowing
