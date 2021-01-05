ca2e341b938d74c29283668b0c33de6257c32bf6
1176325 <1176325@gitlab.lasse.ufpa.br>
2021-01-05 11:03:22 -0300

    improved comments

--- MatlabOctaveCodeSnippets/snip_systems_sos_parameters.m
+++ MatlabOctaveCodeSnippets/snip_systems_sos_parameters.m
@@ -9,4 +9,8 @@ ts_factor= -log(epsilon * sqrt(1-zeta^2))/(zeta) %zeta only
 ts=ts_factor/wn %settling time
 ov=exp(-(zeta*pi)/sqrt(1-zeta^2)) %overshoot (depends on zeta only)
 bw_factor=sqrt(1-2*zeta^2 + sqrt(2 - 4*zeta^2 + 4*zeta^4)) %zeta only
-bw_r=wn*bw_factor %bandwidth in radians/second
+wc=wn*bw_factor %cutoff frequency = 3-dB bandwidth (in radians/second)
+%% Check whether the cuttoff frequency wc corresponds to a -3 dB gain
+s = 1j*wc %define s = j wc
+Hs = (wn^2) / (s^2 + 2*zeta*wn*s + (wn^2)) % system function
+gain_at_wc = 20*log10(abs(Hs))  %answer is -3.0103 dB
