40dfa1c550194405d2c4ccdf60d6e860178aaf85
1176325 <1176325@gitlab.lasse.ufpa.br>
2022-01-09 19:46:15 -0300

    Shows a simple quantizer implementation

---
+++ MatlabOctaveCodeSnippets/snip_signals_nonuniform_quantization.m
@@ -0,0 +1,14 @@
+x=-5
+if x < -0.5
+    if x < -2.5
+        x_quantized = -4
+    else
+        x_quantized = -1
+    end
+else
+    if x < 1.5
+        x_quantized = 0
+    else
+        x_quantized = 3
+    end
+end
