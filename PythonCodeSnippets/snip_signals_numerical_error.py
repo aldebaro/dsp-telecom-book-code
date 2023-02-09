e1c6322497e87543c83805095bf5d7742985ad94
Ryan <79735222+ASTRAson@users.noreply.github.com>
2023-02-09 11:03:23 -0300

    Update snip_signals_numerical_error.py
    Code was already working as intended, only minor changes were made to comply with the "python_template"

--- PythonCodeSnippets/snip_signals_numerical_error.py
+++ PythonCodeSnippets/snip_signals_numerical_error.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-a = np.float64(0)
+a = np.float64(0) # a is defined as a float with a precision of 64 bits after the decimal point
 for i in range(20):
-    a += np.float64(0.1)
-print(a == 2)
-print(np.spacing(np.float64(1)))
-print(np.abs(a) - 2 < np.spacing(np.float64(1)))
+    a += np.float64(0.1) # 20 times  0.1  should be equal to 2
+print(a) # Final value of a
+print(bool(a == 2)) # checking if a is 2 returns False due to numerical error
