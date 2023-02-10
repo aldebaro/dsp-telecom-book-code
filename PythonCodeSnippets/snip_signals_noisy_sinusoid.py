14feb426d5ae6824ebd9e9cb047767fb1bdf54f8
Valdinei Rodrigues <49418863+Manky0@users.noreply.github.com>
2023-02-10 10:52:18 -0300

    Comply with Octave code and LASSE template
    Small changes in used values, to be the same as Octave code.
    Added plot labels.

--- PythonCodeSnippets/snip_signals_noisy_sinusoid.py
+++ PythonCodeSnippets/snip_signals_noisy_sinusoid.py
@@ -1,36 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pylab as pl
-import scipy.signal as sp
 from numpy import random as rd
 
 A = 4  # sinusoid amplitude
 noisePower = 25  # noise power
 f = 2  # frequency in Hz
-n = np.arange(0, 4000)  # Discrete_time
+n = np.arange(0, 3999)  # discrete-time, using many samples to get good estimate
 Fs = 20  # sampling frequency
-x = A * np.sin(2 * np.pi * f / Fs * n)  # generate discrete_time sine
+x = A * np.sin(2 * np.pi * f / Fs * n)  # generate discrete-time sine
 z = np.sqrt(noisePower) * rd.randn(np.size(x))  # generate noise
+
 plt.figure()
 plt.subplot(211)
-plt.stem(x + z)
+plt.plot(x + z)
+
 maxSample = 100  # determine the zoom range
 plt.subplot(212)
-plt.stem(x[0:maxSample] + z[0:maxSample])
+plt.stem(x[1:maxSample] + z[1:maxSample])
 pl.show()
 
-maxlags = 20  # maximum lag for acorr calculation
-# acor signal only
+maxlags = 20  # maximum lag for xcorr calculation
+# signal only
 Hx = plt.xcorr(x, x, usevlines=False, normed=False, maxlags=maxlags)
 lx = Hx[0]
 Rx = Hx[1]
 Rx /= x.size - abs(lx)
-# acor noise only
+# noise only
 Hz = plt.xcorr(z, z, usevlines=False, normed=False, maxlags=maxlags)
 lz = Hz[0]
 Rz = Hz[1]
 Rz /= z.size - abs(lz)
-# acor noise signal
+# noise signal
 Hy = plt.xcorr(x + z, x + z, usevlines=False, normed=False, maxlags=maxlags)
 ly = Hy[0]
 Ry = Hy[1]
@@ -38,12 +39,15 @@ Ry /= (x + z).size - abs(ly)
 
 plt.figure
 plt.subplot(311)
-plt.stem(lx, Rx / 2500)
+plt.ylabel('R_x[l]')
+plt.stem(lx, Rx)
 
 plt.subplot(312)
-plt.stem(lz, Rz / 5000)
+plt.ylabel('R_z[l]')
+plt.stem(lz, Rz)
 
 plt.subplot(313)
-plt.stem(ly, Ry / 4000)
-
+plt.stem(ly, Ry)
+plt.xlabel('Lag l')
+pl.ylabel('R_y[l]')
 pl.show()
