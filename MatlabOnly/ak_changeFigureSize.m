ac6c0f97b134624f1e98ebc2ef41d268665ee0a6
1176325 <1176325@gitlab.lasse.ufpa.br>
2023-02-19 11:10:37 -0300

    Change figure size

---
+++ MatlabOnly/ak_changeFigureSize.m
@@ -0,0 +1,14 @@
+function ak_changeFigureSize(width_factor, height_factor)
+% function ak_changeFigureSize(width_factor, height_factor)
+%Change figure size by the factors width_factor and height_factor,
+%and position figure in center of screen.
+
+%From https://www.cs.cornell.edu/courses/cs100m/2007fa/Graphics/Position.pdf
+%set(gcf,'position',[a b H W])
+%places the lower left corner of an H-by-W figure window at (a, b).
+
+pos=get(gcf, 'Position'); %get figure's position on screen
+pos(3)=floor(pos(3)*height_factor); %adjust the figure size
+pos(4)=floor(pos(4)*width_factor); %adjust the figure size
+set(gcf,'Position',pos);
+movegui(gcf,'center')
