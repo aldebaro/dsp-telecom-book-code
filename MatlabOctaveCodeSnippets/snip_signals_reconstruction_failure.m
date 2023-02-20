feca7bc9d77558568aaaf8bdd89e3ae0dbdff74a
1176325 <1176325@gitlab.lasse.ufpa.br>
2023-02-20 17:39:38 -0300

    reconstruction and sampling and even-odd

--- MatlabOctaveCodeSnippets/snip_signals_reconstruction_failure.m
+++ MatlabOctaveCodeSnippets/snip_signals_reconstruction_failure.m
@@ -1,20 +1,19 @@
 %% Define variables
-max_n = 8; %n varies from -max_n to max_n
 Ts=0.2; %sampling interval (in seconds)
-A=4; %cosine amplitude, in Volts
 Fs=1/Ts; %sampling frequency (5 Hz)
-fc=Fs/4; %cosine frequency (1.25 Hz)
 oversampling_factor = 200; %oversampling factor
 textra = 1; %one extra time (1 second) for visualizing sincs
+pulse_width = 0.2; %support of the rects in seconds
 %% Generate signal xn sampled at Fs
-n=-max_n:max_n; %original discrete-time axis as integers
-t=(-max_n:max_n)*Ts; %original sampled time axis in seconds
-xn=A*cos(2*pi*fc*t); %cosine sampled at Fs
+n=-3:3;
+xn=[0 0 0 1 -3 3 0]; %signal with only three non-zero samples
 %% Generate oversampled version of xn
 oversampled_Ts = Ts/oversampling_factor; %new value of Ts
 oversampled_n = n(1)*oversampling_factor:n(end)*oversampling_factor;
 oversampled_t = oversampled_n*oversampled_Ts; %time in seconds
-oversampled_xn=A*cos(2*pi*fc*oversampled_t); %oversampled cosine
+oversampled_xn = rectpuls(oversampled_t,pulse_width) - ...
+    3*rectpuls(oversampled_t-Ts,pulse_width) + ...
+    3*rectpuls(oversampled_t-2*Ts,pulse_width); %define the signal
 %% Reconstruct signal from samples stored at xn and compare with 
 %% the "ground truth" oversampled_xn
 ak_sinc_reconstruction(n,xn,Ts,oversampled_n,oversampled_xn,textra);
