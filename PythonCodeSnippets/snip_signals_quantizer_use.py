ef006c1a1d95e5b1e4ceba4f709ff96a8614413d
jvictor <jvictorsbferreira@gmail.com>
2023-11-04 11:30:36 -0300

    resolve merge conflict in PythonCodeSnippets/snip_signals_quantizer_use.py

--- PythonCodeSnippets/snip_signals_quantizer_use.py
+++ PythonCodeSnippets/snip_signals_quantizer_use.py
@@ -1,11 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pylab as pl
 from lasse.dsp.scalar_quantization import ak_quantizer
->>>>>>> upstream/master
 
 
 delta = 0.5  # quantization step
-b = 3  # Number of bits
-x = np.arange(-5, 4, 1 * 10**-2)  # Define input dynamic range
+b = 3  # Number od bits
+x = np.arange(-5, 4, 1 * 10 ** -2)  # Define input dynamic range
 x_i, x_q = ak_quantizer(x, delta, b)  # Do the quantization
 
 plt.plot(x, x_q, color="red")
