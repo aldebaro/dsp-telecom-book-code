6d068ec092590b1ecfcb36d3c79041cf697ae050
Aldebaro Klautau <aldebaro@ufpa.br>
2023-11-24 15:49:34 -0300

    Do not use approximation

--- MatlabOctaveCodeSnippets/snip_systems_notch.m
+++ MatlabOctaveCodeSnippets/snip_systems_notch.m
@@ -1,8 +1,8 @@
 f_notch=60.0; % notch frequency in Hz
-BW_3dB=4.0;  % 3 dB bandwidth in Hz
+BW_3dB=4;  % 3 dB bandwidth in Hz
 Fs=400; % sampling frequency in Hz
 Omega_0=2*pi*(f_notch/Fs); % digital angular frequency
-r=1-(BW_3dB/Fs)*pi; % poles radius
+r=exp(-(BW_3dB/Fs)*pi); % poles radius
 notch_zeros = [1*exp(1i*Omega_0) 1*exp(-1i*Omega_0)];                
 notch_poles = [r*exp(1i*Omega_0) r*exp(-1i*Omega_0)];
 Bz_notch=poly(notch_zeros); % numerator B(z)
