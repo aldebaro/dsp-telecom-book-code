481fd6948238efc277e1049c7e693d5349f9787f
Aldebaro Klautau <aldebaro@ufpa.br>
2023-12-30 15:40:40 -0300

    Cleaning wavread and wavwrite, deprecated

--- MatlabOctaveCodeSnippets/snip_frequency_narrow_wide_spec.m
+++ MatlabOctaveCodeSnippets/snip_frequency_narrow_wide_spec.m
@@ -1,6 +1,8 @@
-[s,Fs,numbits]=readwav('WeWereAway.wav'); %read wav file
+[s,Fs,wmode,fidx]=readwav('WeWereAway.wav','r'); %read wav file
+numbits = fidx(7); % num of bits per sample (should be 16)
 Nfft = 1024; %number of FFT points
-M=64; %window length
-specgram(s,Nfft,Fs,hann(M),round(3/4*M)); colorbar, pause
-M=256;specgram(s,Nfft,Fs,hann(M),round(3/4*M));colorbar
+figure(1), M=64; %window length in samples for wideband
+specgram(s,Nfft,Fs,hann(M),round(3/4*M)); colorbar
+figure(2), M=256; %window length in samples for narrowband
+specgram(s,Nfft,Fs,hann(M),round(3/4*M)); colorbar
 
--- MatlabOnly/snip_signals_wavwrite.m
+++ MatlabOnly/snip_signals_wavwrite.m
@@ -1,5 +1,9 @@
-yd=double(y); %convert from int16 (y from getaudiodata)
-yd=yd/max(abs(yd)); %normalize
-writewav(yd,11025,'somename.wav','16r') %write as 16-bits
+y=[14;50;32767;-32768;14;0]; %example, as column vector
+writewav(y,11025,'somename.wav','16r') %write as 16-bits
 z=readwav('somename.wav','r'); %avoid normalization
-
+isequal(y,z) %should return true
+% compare with double representation
+yd=double(y); %convert from int16
+yd=yd/max(abs(yd)); %normalize
+zd=readwav('somename.wav','s'); %with normalization
+isequal(yd,zd) %should return true
