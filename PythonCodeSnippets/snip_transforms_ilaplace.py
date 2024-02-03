8a0611d410d446bddc5737862cfd237300f90865
LucasDamascenoS <lucasdamasceno484@gmail.com>
2024-02-03 01:28:39 -0300

    Fixing Euler representation

--- PythonCodeSnippets/snip_transforms_ilaplace.py
+++ PythonCodeSnippets/snip_transforms_ilaplace.py
@@ -1,12 +1,14 @@
 import sympy as sp
 
-s = sp.symbols('s')
 t = sp.Symbol('t', positive=True)
+s = sp.symbols('s')
+
 a = 1
-b = -2
+b = -1
 c = -1 + 2*sp.I
 
 X = (s - a) / ((s - b) * (s - c) * (s - sp.conjugate(c)))
 
-ilaplace_X = sp.inverse_laplace_transform(X, s, t)
-print(ilaplace_X)
+X_ilaplace = sp.inverse_laplace_transform(X, s, t)
+X_ilaplace = sp.expand(X_ilaplace.rewrite(sp.exp))
+print(X_ilaplace)
