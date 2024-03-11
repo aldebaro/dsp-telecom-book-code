7815fb05954e44bfbb3d029cc2d5f164f2a7df0a
fritz2581 <genivaldo.silva@itec.ufpa.br>
2024-03-11 14:13:13 -0300

    Change range of t vector and add plt.show to show the graph

--- PythonCodeSnippets/snip_signals_sinc.py
+++ PythonCodeSnippets/snip_signals_sinc.py
@@ -5,6 +5,7 @@ from scipy.special import sinc
 Ts = 0.001  # sampling interval (in seconds)
 xi = 0.2  # support of the sinc in seconds
 gamma = 0.5  # time shift
-t = np.arange(-1.6, 1.6, Ts)  # define discrete-time axis with fine resolution
+t = np.arange(-1.6, 1.6+Ts, Ts)  # define discrete-time axis with fine resolution
 x = 3 * sinc((t - gamma) / xi)  # define the signal x(t)
 plt.plot(t, x)
+plt.show()
