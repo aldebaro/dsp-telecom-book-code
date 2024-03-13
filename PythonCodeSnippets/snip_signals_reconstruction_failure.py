aca7f0337bf367f2c88591d2b0ccb99d9a8157b7
fritz2581 <genivaldo.silva@itec.ufpa.br>
2024-03-13 15:31:08 -0300

    add sinc_Rec fuction and fix n_oversampled index

--- PythonCodeSnippets/snip_signals_reconstruction_failure.py
+++ PythonCodeSnippets/snip_signals_reconstruction_failure.py
@@ -1,20 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from lasse.util.python_as_matlab import rectpuls
-from lasse.util.ak_functions import ak_sinc_reconstruction
+import sincRec_fuction as sR
+
+def rectpuls(t, width):
+    """Generates a rectangular pulse with specified width."""
+    return np.where(np.abs(t) <= width / 2, 1, 0)
 
 ## Define variables
 Ts=0.2 #sampling interval (in seconds)
 Fs=1/Ts #sampling frequency (5 Hz)
 oversampling_factor = 200 #oversampling factor
-textra = 1 #one extra time (1 second) for visualizing sincs
+textra = 0 #one extra time (1 second) for visualizing sincs
 pulse_width = 0.2 #support of the rects in seconds
+graph_overlay = True #allows you to plot overlapping graphs
 ## Generate signal xn sampled at Fs
 n= np.arange(-3,3+1)
 xn=np.array([0, 0, 0, 1, -3, 3, 0]) #signal with only three non-zero samples
 ## Generate oversampled version of xn
 oversampled_Ts = Ts/oversampling_factor #new value of Ts
-oversampled_n = np.arange(n[0]*oversampling_factor,n[-1]*oversampling_factor)
+oversampled_n = np.arange(n[0]*oversampling_factor,n[-1]*oversampling_factor+1)
 oversampled_t = oversampled_n*oversampled_Ts #time in seconds
 
 oversampled_xn = rectpuls(oversampled_t,pulse_width) - \
@@ -22,5 +26,4 @@ oversampled_xn = rectpuls(oversampled_t,pulse_width) - \
                 3*rectpuls(oversampled_t-2*Ts,pulse_width) #define the signal
 ## Reconstruct signal from samples stored at xn and compare with 
 ## the "ground truth" oversampled_xn
-ak_sinc_reconstruction(n,xn,Ts,oversampled_n,oversampled_xn,textra)
-plt.show()
+a=sR.ak_sinc_reconstruction(n,xn,Ts,oversampled_n,oversampled_xn,textra,graph_overlay)
