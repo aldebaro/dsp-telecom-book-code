880c72326a6500fbf294417b448cfd90ab23aca0
Aldebaro Klautau <aldebaro@ufpa.br>
2024-06-26 08:17:21 -0300

    added figures

--- MatlabBookFigures/figs_spectral_windows.m
+++ MatlabBookFigures/figs_spectral_windows.m
@@ -1,6 +1,7 @@
 %% Plots illustrating leakage
 % Assume pwd is something like 
 % C:\ak_bootor\dsp-telecom-book-code\MatlabOctaveFunctions
+function figs_spectral_windows()
 outputFolder = '../../latex/ak_dspbook/Figures/';
 W0 = 1; % signal angular frequency in rad
 A = 3; % signal amplitude in V
@@ -430,3 +431,60 @@ ylabel('Normalized dB Magnitude');
 plot([-pi:2*pi/N:pi-2*pi/N],-100*[ones(1,N)],'ko','markersize',12);
 plot(w,filter_response(:,3),'rx','LineWidth',3);
 writeEPS('dftFilterBankKaiser','font12Only')
+
+window_choice = 1; %choose one among 3 possible windows
+alpha=8; %specifies the cosine frequency
+plot_fft_with_impulses(window_choice, alpha)
+writeEPS('scalloping_rect_centered','font12Only')
+
+window_choice = 3; %choose one among 3 possible windows
+alpha=8; %specifies the cosine frequency
+plot_fft_with_impulses(window_choice, alpha)
+writeEPS('scalloping_flattop_centered','font12Only')
+
+window_choice = 1; %choose one among 3 possible windows
+alpha=8.5; %specifies the cosine frequency
+plot_fft_with_impulses(window_choice, alpha)
+writeEPS('scalloping_rect_noncentered','font12Only')
+
+window_choice = 2; %choose one among 3 possible windows
+alpha=8.5; %specifies the cosine frequency
+plot_fft_with_impulses(window_choice, alpha)
+writeEPS('scalloping_hann_noncentered','font12Only')
+
+window_choice = 3; %choose one among 3 possible windows
+alpha=8.5; %specifies the cosine frequency
+plot_fft_with_impulses(window_choice, alpha)
+writeEPS('scalloping_flattop_noncentered','font12Only')
+
+
+%% Function to plot. Same as script snip_frequency_scalloping.m
+function plot_fft_with_impulses(window_choice, alpha)
+clf, N=32; A=6; %clear figure, FFT length, cosine amplitude
+Wc=(alpha*2*pi)/N; %cosine frequency in radians
+n=0:N-1; x=A*cos(Wc*n); %generate N samples of the cosine
+switch (window_choice)
+  case 1
+    this_window = rectwin(N);
+  case 2
+    this_window = hanning(N);
+  case 3
+  this_window = flattopwin(N);
+end
+amplitude_scaling = sum(this_window); %factor to mitigate scalloping
+xw = x.* transpose(this_window); %multiply in time-domain
+Xw_scaled_fft = fft(xw)/amplitude_scaling; %N-points FFT and scale it
+max_fft_amplitude = max(abs(Xw_scaled_fft));
+disp(['Max(abs(scaled FFT))=' num2str(max_fft_amplitude )])
+scalloping_loss=(A/2)-max_fft_amplitude;
+disp(['Scalloping loss = ' num2str(scalloping_loss)])
+disp(['Correct amplitude (Volts) = ' num2str(A)])
+disp(['Estimated amplitude (Volts) = ' num2str(2*max_fft_amplitude)])
+amplitude_error = A - 2*max_fft_amplitude;
+disp(['Amplitude error (Volts) = ' num2str(amplitude_error)])
+disp(['Amplitude error (%) = ' num2str(100*amplitude_error/A)])
+ak_impulseplot([A/2, A/2],[Wc,2*pi-Wc]/pi,[]); %plot cosine impulses
+hold on, stem([0:N-1]*(2*pi/N)/pi,abs(Xw_scaled_fft),'or')
+xlabel('Angular frequency \Omega (rad) normalized by \pi')
+ylabel('Spectrum magnitude'), grid
+legend('DTFT (impulse area divided by \pi)', 'FFT magnitude')
