35c8c6b709a54fed8ccfcd355efe374219eb9dcd
Aldebaro Klautau <aldebaro@ufpa.br>
2024-06-26 08:17:36 -0300

    fixed axes

--- MatlabOctaveCodeSnippets/snip_frequency_scalloping.m
+++ MatlabOctaveCodeSnippets/snip_frequency_scalloping.m
@@ -1,6 +1,6 @@
 window_choice = 1; %choose one among 3 possible windows
-clf, N=32; A=6; %clear figure, FFT length, cosine amplitude
 alpha=8.5; %specifies the cosine frequency
+clf, N=32; A=6; %clear figure, FFT length, cosine amplitude
 Wc=(alpha*2*pi)/N; %cosine frequency in radians
 n=0:N-1; x=A*cos(Wc*n); %generate N samples of the cosine
 switch (window_choice)
@@ -15,7 +15,7 @@ amplitude_scaling = sum(this_window); %factor to mitigate scalloping
 xw = x.* transpose(this_window); %multiply in time-domain
 Xw_scaled_fft = fft(xw)/amplitude_scaling; %N-points FFT and scale it
 max_fft_amplitude = max(abs(Xw_scaled_fft));
-disp(['Max(abs(scaled FFT))=' num2str(max_fft_amplitude )])
+disp(['Max(abs(scaled FFT)) = ' num2str(max_fft_amplitude )])
 scalloping_loss=(A/2)-max_fft_amplitude;
 disp(['Scalloping loss = ' num2str(scalloping_loss)])
 disp(['Correct amplitude (Volts) = ' num2str(A)])
@@ -25,5 +25,6 @@ disp(['Amplitude error (Volts) = ' num2str(amplitude_error)])
 disp(['Amplitude error (%) = ' num2str(100*amplitude_error/A)])
 ak_impulseplot([A/2, A/2],[Wc,2*pi-Wc]/pi,[]); %plot cosine impulses
 hold on, stem([0:N-1]*(2*pi/N)/pi,abs(Xw_scaled_fft),'or')
-xlabel('Frequency \Omega (rad) normalized by \pi)')
-ylabel('FFT magnitude and impulse area scaled by \pi'), grid
+xlabel('Angular frequency \Omega (rad) normalized by \pi')
+ylabel('Spectrum magnitude'), grid
+legend('DTFT (impulse area divided by \pi)', 'FFT magnitude')
