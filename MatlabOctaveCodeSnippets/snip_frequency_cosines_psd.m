e865c664acd7dacaee3f6d2327afb2ac9d3a7709
Aldebaro Klautau <aldebaro@ufpa.br>
2024-06-29 23:34:26 -0300

    psd of 2 cosines

---
+++ MatlabOctaveCodeSnippets/snip_frequency_cosines_psd.m
@@ -0,0 +1,15 @@
+N=128; n=(0:N-1)'; %number of samples and column vector
+Fs=2000; %bandwidth in Hertz, equivalent to sampling frequency
+dW=(2*pi)/N; %FFT bin width in radians, to define cosines
+k1=N/4; k2=N/8; W1=k1*dW; W2=k2*dW;%define frequencies in radians
+x=10*cos(W1*n) + 1*cos(W2*n); %create sum of bin-centered cosines
+Sdef = (abs(fft(x)).^2)/(N*Fs); %periodogram via its definition
+Sdef = [Sdef(1); 2*Sdef(2:N/2); Sdef(N/2+1)]; %convert to unilateral
+[S,w] = periodogram(x,rectwin(N),N,Fs); %using periodogram function
+Power_freq=(Fs/N)*sum(S) %estimate power (watts) in frequency domain
+Power_time=mean(x.^2) %estimate power (watts) in time domain
+h=plot(w,10*log10(Sdef),'-x'); hold on
+h2=plot(w,10*log10(S),'-ro'); %Periodogram (dB scale)
+legend('via definition','via Matlab')
+xlabel('f (Hz)'), ylabel('S(f) (dBW/Hz)')
+datatip(h,W1*Fs/(2*pi),Sdef(k1)), datatip(h,W2*Fs/(2*pi),Sdef(k2))
